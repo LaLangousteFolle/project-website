002256144c53be7e7d6824d660630b4b29408871
Noe SEILER <seilerlebed@gmail.com>
2025-11-30 10:24:54 +0100

    fixed haj rotation :3

--- components/Shark3D.tsx
+++ components/Shark3D.tsx
@@ -5,13 +5,13 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Suspense } from "react";
 
 function SharkModel() {
-  const { scene } = useGLTF("/models/shark.glb"); // chemin public
+  const { scene } = useGLTF("/models/shark.glb");
   return (
     <primitive
       object={scene}
-      scale={1.2}
+      scale={1.0}
       rotation={[0, Math.PI / 2, 0]}
-      position={[0, -0.3, 0]}
+      position={[0.3, -0.45, 0]} 
     />
   );
 }
@@ -19,13 +19,18 @@ function SharkModel() {
 export function Shark3D() {
   return (
     <div className="w-64 h-64 rounded-xl bg-slate-900/60 border border-cyan-500/30 shadow-lg overflow-hidden">
-      <Canvas camera={{ position: [0, 0.5, 3], fov: 40 }}>
+      <Canvas camera={{ position: [0, 0.3, 4], fov: 50 }}>
         <ambientLight intensity={0.7} />
         <directionalLight position={[3, 3, 3]} intensity={1.2} />
         <Suspense fallback={null}>
           <SharkModel />
         </Suspense>
-        <OrbitControls enableZoom={false} enablePan={false} autoRotate autoRotateSpeed={1.2} />
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          autoRotate
+          autoRotateSpeed={10}
+        />
       </Canvas>
     </div>
   );
